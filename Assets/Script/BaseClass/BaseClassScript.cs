0f3aa610c559333290b546aa4a7a0cf284e0011e
Erwann Charlier <erwann.charlier@outlook.be>
2025-11-22 12:48:34 +0100

    fin cours 2

---
+++ Assets/Script/BaseClass/BaseClassScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaseClassScript : MonoBehaviour
+{
+    [Header("Stats")]
+    public string ClassName;
+    public int Health;
+    public int Strength;
+    public int Intelligence;
+    public int Agility;
+    public int Damage;
+    public bool canShoot;
+}
